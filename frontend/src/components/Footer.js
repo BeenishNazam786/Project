e2cf5a9f2fb973464a94120e87bd16b68148efe8
BeenishNazam786 <beenish.nazam786@gmail.com>
2024-12-01 23:29:33 +0200

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,6 +10,10 @@ const Footer = () => {
         <a href="#" className="social-link">Facebook</a> |
         <a href="#" className="social-link">Instagram</a>
       </div>
+    <div className="contact-us">
+        <p>Contact us at:
+          <a href="tel:+1234567890" className="phone-link">+123 456 7890</a></p>
+      </div>
     </footer>
   );
 };
