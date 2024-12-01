ad7df5689a8f5a99bed958cb30e1fb9575b04fa9
BeenishNazam786 <beenish.nazam786@gmail.com>
2024-12-01 23:40:32 +0200

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,12 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>© 2024 Blood Bank. All Rights Reserved.</p>
-     
+     <div>
+        Follow us:
+        <a href="#" className="social-link">Facebook</a> |
+        <a href="#" className="social-link">Instagram</a>
+      </div>
+
     <div className="contact-us">
         <p>Contact us at:
           <a href="tel:+1234567890" className="phone-link">+123 456 7890</a></p>
